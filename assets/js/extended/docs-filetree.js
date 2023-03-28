f180d425cb7fc8ad12829402a3852442d85dfa52
iliajie <gpg@ilia.engineer>
2023-03-28 23:09:32 +0300

    Add scroll to in Docs nav menu (left links)

--- assets/js/extended/docs-filetree.js
+++ assets/js/extended/docs-filetree.js
@@ -1,18 +1,29 @@
-// Attach docs filetree menu to the right
-// container depending on the screen size
-window.addEventListener("resize", function(e) {
-    const asideMenu = document.querySelector('.docs-menu'),
-        asideMobile = document.querySelector('.asideMobile .inner');
+(function() {
+    // Attach docs filetree menu to the right
+    // container depending on the screen size
+    window.addEventListener("resize", function(e) {
+        const asideMenu = document.querySelector('.docs-menu'),
+            asideMobile = document.querySelector('.asideMobile .inner');
 
-    // Desktop mode
-    if (asideMenu && asideMenu.offsetParent) {
-        const contentActiveContent = asideMobile.querySelector('.docs-menu-content');
-        contentActiveContent && asideMenu.append(contentActiveContent)
-    // Mobile mode
-    } else {
-        const contentActiveContent = asideMenu.querySelector('.docs-menu-content');
-        contentActiveContent && asideMobile.append(contentActiveContent)
-    }
-    document.dispatchEvent(new Event("scroll"));
-});
-window.dispatchEvent(new Event("resize"));
+        // Desktop mode
+        if (asideMenu && asideMenu.offsetParent) {
+            const contentActiveContent = asideMobile.querySelector('.docs-menu-content');
+            contentActiveContent && asideMenu.append(contentActiveContent)
+            // Mobile mode
+        } else {
+            const contentActiveContent = asideMenu.querySelector('.docs-menu-content');
+            contentActiveContent && asideMobile.append(contentActiveContent)
+        }
+        document.dispatchEvent(new Event("scroll"));
+    });
+    window.dispatchEvent(new Event("resize"));
+
+    // Scroll filetree into view
+    const closestDetails = document.querySelector('.docs-menu details .active');
+    closestDetails && closestDetails.closest('details').setAttribute('open', 'open');
+    setTimeout(function() {
+        if (closestDetails && !isVisible(closestDetails)) {
+            closestDetails.scrollIntoView({ behavior: "smooth", block: 'nearest', inline: 'nearest' });
+        }
+    }, (3e1 + 4e1) * 3)
+})();
--- assets/js/extended/docs-toc.js
+++ assets/js/extended/docs-toc.js
@@ -19,55 +19,7 @@
                 })[0]]
             }
             return null
-        }).filter(function(e) { return e !== null })),
-        isVisible = function(elem) {
-            if (!(elem instanceof Element)) return null;
-            const style = getComputedStyle(elem);
-            if (style.display === 'none') return false;
-            if (style.visibility !== 'visible') return false;
-            if (style.opacity === 0) return false;
-            if (elem.offsetWidth + elem.offsetHeight + elem.getBoundingClientRect().height +
-                elem.getBoundingClientRect().width === 0) {
-                return false;
-            }
-            var elementPoints = {
-                'center': {
-                    x: elem.getBoundingClientRect().left + elem.offsetWidth / 2,
-                    y: elem.getBoundingClientRect().top + elem.offsetHeight / 2
-                },
-                'top-left': {
-                    x: elem.getBoundingClientRect().left,
-                    y: elem.getBoundingClientRect().top
-                },
-                'top-right': {
-                    x: elem.getBoundingClientRect().right,
-                    y: elem.getBoundingClientRect().top
-                },
-                'bottom-left': {
-                    x: elem.getBoundingClientRect().left,
-                    y: elem.getBoundingClientRect().bottom
-                },
-                'bottom-right': {
-                    x: elem.getBoundingClientRect().right,
-                    y: elem.getBoundingClientRect().bottom
-                }
-            }
-
-            for (index in elementPoints) {
-                var point = elementPoints[index];
-                if (point.x < 0) return false;
-                if (point.x > (document.documentElement.clientWidth || window.innerWidth)) return false;
-                if (point.y < 0) return false;
-                if (point.y > (document.documentElement.clientHeight || window.innerHeight)) return false;
-                let pointContainer = document.elementFromPoint(point.x, point.y);
-                if (pointContainer !== null) {
-                    do {
-                        if (pointContainer === elem) return true;
-                    } while (pointContainer = pointContainer.parentNode);
-                }
-            }
-            return false;
-        };
+        }).filter(function(e) { return e !== null }));
 
     let $this = {};
     labelTarget.querySelectorAll('ul > li > a').forEach(function(elem) {
