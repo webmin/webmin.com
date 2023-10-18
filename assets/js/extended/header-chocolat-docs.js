8fb7c1ab5f4e2a542df8bad6318f1d0802a87f9d
Ilia Ross <ilia@ross.gdn>
2023-10-18 15:49:06 +0300

    Add lightbox support for screenshots in docs

---
+++ assets/js/extended/header-chocolat-docs.js
@@ -0,0 +1,34 @@
+window.addEventListener("DOMContentLoaded", () => {
+    setTimeout(function () {
+        Chocolat(document.querySelectorAll('.post-content a[href*="/screenshots/"]'), {
+            allowFullScreen: false,
+            imageSize: "contain",
+            loop: false,
+            fullScreen: false,
+            imageSource: "href",
+            allowZoom: false,
+            imageSize: "scale-down",
+            afterInitialize: function afterInitialize() {
+                document.documentElement.classList.add("overflow-hidden");
+                document.querySelector(".header").classList.add("no-z-index");
+            },
+            afterClose: function afterClose() {
+                document.documentElement.classList.remove("overflow-hidden");
+                document.querySelector(".header").classList.remove("no-z-index");
+            },
+        });
+        document.addEventListener("keydown", function (event) {
+            if (event.key === "Escape" && document.body.classList.contains("chocolat-open")) {
+                event.preventDefault();
+                document.querySelector(".chocolat-layout .chocolat-close").click();
+            }
+        });
+        document.querySelector("#theme-toggle").addEventListener(
+            "click",
+            function () {
+                location.reload();
+            },
+            false
+        );
+    }, 100);
+});
