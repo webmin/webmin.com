0a955819d8e61eadfe478b972f0d18bd7ebedbfd
Ilia Ross <ilia@ross.gdn>
2023-11-18 23:10:40 +0200

    Add further improvements to TOC scrolling

--- assets/js/extended/docs-toc.js
+++ assets/js/extended/docs-toc.js
@@ -43,114 +43,129 @@
     if (scrollTarget && labelTarget) {
         document.addEventListener(
             "scroll",
-            function (e) {
+            function () {
                 const execScroll = function () {
-                        // Stop if hidden
-                        if (!document.querySelector(labelTargetSelStr).offsetParent) {
-                            return;
-                        }
-
-                        // Scroll direction
-                        let scrollDirection = "up",
-                            st = window.pageYOffset || document.documentElement.scrollTop;
-                        if (st > $this.lastScrollTop) {
-                            scrollDirection = "down";
-                        }
-                        $this.lastScrollTop = st <= 0 ? 0 : st;
-
-                        // Accommodate header offset
-                        const scrollOffset = document.querySelector(".header").clientHeight / 2,
-                            scrollDifference =
-                                document.documentElement.scrollHeight - window.innerHeight,
-                            scrollposition = document.documentElement.scrollTop,
-                            labelTargetsClear = function () {
-                                labelTargets.forEach(function (e) {
-                                    e[0].classList.remove("active");
-                                });
-                            };
-                        if (scrollDifference - scrollposition <= 2) {
-                            labelTargetsClear();
-                            labelTargets[labelTargets.length - 1][0].classList.add("active");
-                        } else {
-                            let lastLabelElem;
-                            for (const [curElem, labelElem, curID] of dataArray) {
-                                if (window.scrollY + scrollOffset >= curElem.offsetTop) {
-                                    labelTargetsClear();
-                                    labelElem.classList.add("active");
-                                    lastLabelElem = labelElem;
-                                    continue;
-                                }
-                                labelElem.classList.remove("active");
-                            }
-
-                            // Scroll into view the content on main page scroll
-                            if (typeof $this.scrollTimer === "number")
-                                clearTimeout($this.scrollTimer);
-                            if (typeof $this.scrollLastElem === "number")
-                                clearTimeout($this.scrollLastElem);
-                            if (lastLabelElem) {
-                                $this.scrollLastElem = window.setTimeout(
-                                    function () {
-                                        const isVisibleElem = isVisible(lastLabelElem);
-                                        $this.scrollTimer = window.setTimeout(
-                                            function () {
-                                                let elemParent = lastLabelElem.parentNode,
-                                                    elemsCont = labelTarget.querySelector("ul"),
-                                                    elemsContTitle =
-                                                        labelTarget.querySelector("h4"),
-                                                    elemIndex = Array.prototype.indexOf.call(
-                                                        elemsCont.childNodes,
-                                                        elemParent
-                                                    );
-                                                if (!isVisibleElem) {
-                                                    if (elemIndex === 0) {
-                                                        elemParent = elemsContTitle;
-                                                    }
-                                                    elemParent.scrollIntoView({
-                                                        behavior: "instant",
-                                                        block:
-                                                            scrollDirection === "up"
-                                                                ? "end"
-                                                                : "start",
-                                                        inline: "center",
-                                                    });
-                                                }
-                                                setTimeout(function () {
-                                                    $this.scrollDelay = 0;
-                                                }, 3e1 + 4e1);
-                                            },
-                                            $this.scrollDelay ? 3e1 : 0
-                                        );
-                                    },
-                                    $this.scrollDelay ? 4e1 : 0
-                                );
-                            }
-                        }
-                        // Set correct link hash
-                        const activeLink = labelTarget.querySelector("a.active");
-                        if (activeLink) {
-                            const activeLinkHash = activeLink.getAttribute("href");
-                            if (activeLinkHash && activeLinkHash !== window.location.hash) {
-                                window.history.replaceState(
-                                    null,
-                                    null,
-                                    window.location.pathname + activeLinkHash
-                                );
-                            }
-                        }
-                    };
-                // When scrolling stopped
-                if ($this.scrollDelay) {
-                    if (this.debounceTimer) {
-                        $this.scrollDelay = 1;
-                        clearTimeout(this.debounceTimer);
+                    // Stop if hidden
+                    if (!document.querySelector(labelTargetSelStr).offsetParent) {
+                        return;
                     }
-                    this.debounceTimer = setTimeout(function () {
+
+                    // Scroll direction
+                    let scrollDirection = "up",
+                        st = window.pageYOffset || document.documentElement.scrollTop;
+                    if (st > $this.lastScrollTop) {
+                        scrollDirection = "down";
+                    }
+                    $this.lastScrollTop = st <= 0 ? 0 : st;
+
+                    // Accommodate header offset
+                    const scrollOffset = document.querySelector(".header").clientHeight / 2,
+                        scrollDifference =
+                            document.documentElement.scrollHeight - window.innerHeight,
+                        scrollposition = document.documentElement.scrollTop,
+                        labelTargetsClear = function () {
+                            labelTargets.forEach(function (e) {
+                                e[0].classList.remove("active");
+                            });
+                        };
+                    if (scrollDifference - scrollposition <= 2) {
+                        labelTargetsClear();
+                        labelTargets[labelTargets.length - 1][0].classList.add("active");
+                    } else {
+                        let lastLabelElem;
+                        for (const [curElem, labelElem, curID] of dataArray) {
+                            if (window.scrollY + scrollOffset >= curElem.offsetTop) {
+                                labelTargetsClear();
+                                labelElem.classList.add("active");
+                                lastLabelElem = labelElem;
+                                continue;
+                            }
+                            labelElem.classList.remove("active");
+                        }
+
+                        // Scroll into view the content on main page scroll
+                        if (typeof $this.scrollTimer === "number") clearTimeout($this.scrollTimer);
+                        if (typeof $this.scrollLastElem === "number")
+                            clearTimeout($this.scrollLastElem);
+                        if (lastLabelElem) {
+                            $this.scrollLastElem = window.setTimeout(
+                                function () {
+                                    const isVisibleElem = isVisible(lastLabelElem);
+                                    $this.scrollTimer = window.setTimeout(
+                                        function () {
+                                            let elemParent = lastLabelElem.parentNode,
+                                                elemsCont = labelTarget.querySelector("ul"),
+                                                elemsContTitle = labelTarget.querySelector("h4"),
+                                                elemIndex = Array.prototype.indexOf.call(
+                                                    elemsCont.childNodes,
+                                                    elemParent
+                                                );
+                                            if (!isVisibleElem) {
+                                                if (elemIndex === 0) {
+                                                    elemParent = elemsContTitle;
+                                                }
+                                                elemParent.scrollIntoView({
+                                                    behavior: "auto",
+                                                    // Glitcy in non-stop scrolling, but why!?
+                                                    block:
+                                                        scrollDirection === "up" ? "end" : "start",
+                                                    inline: "center",
+                                                });
+                                            }
+                                            setTimeout(function () {
+                                                $this.scrollDelay = 0;
+                                            }, 3e1 + 4e1);
+                                        },
+                                        $this.scrollDelay ? 3e1 : 0
+                                    );
+                                },
+                                $this.scrollDelay ? 4e1 : 0
+                            );
+                        }
+                    }
+                };
+                // When scrolling stopped
+                const deferredScroll = (time) => {
+                    $this_ = this;
+                    if ($this_.debounceTimer) {
+                        $this.scrollDelay = 1;
+                        clearTimeout($this_.debounceTimer);
+                    }
+                    $this_.debounceTimer = setTimeout(function () {
                         execScroll();
-                    }, 10e1);
-                    
+                        $this_.scrollCounter = 1;
+                        // Update URL hash
+                        if (document.body.dataset.loaded) {
+                            const activeLink = labelTarget.querySelector("a.active");
+                            if (activeLink) {
+                                const activeLinkHash = activeLink.getAttribute("href");
+                                if (activeLinkHash && activeLinkHash !== window.location.hash) {
+                                    window.history.replaceState(
+                                        null,
+                                        null,
+                                        window.location.pathname + activeLinkHash
+                                    );
+                                }
+                            }
+                        }
+                    }, time);
+                };
+                // If triggered by click, scroll delay us lower than usual
+                if ($this.scrollDelay) {
+                    deferredScroll(10e1);
                 } else {
-                    execScroll();
+                    deferredScroll(50e1);
+                }
+                if (!this.scrollCounter) {
+                    this.scrollCounter = 1;
+                } else {
+                    // After scrolling for a 
+                    // while, update the TOC
+                    this.scrollCounter++;
+                    if (this.scrollCounter > 100) {
+                        this.scrollCounter = 1;
+                        execScroll();
+                    }
                 }
             },
             { passive: true }
@@ -163,6 +178,7 @@
     // Adjust initial scroll position
     window.onload = function () {
         setTimeout(function () {
+            document.body.dataset.loaded = true;
             const hash = window.location.hash;
             if (hash) {
                 const anchoredElement = document.querySelector(hash);
@@ -173,6 +189,6 @@
                     window.scrollTo(0, adjustedPosition);
                 }
             }
-        }, 100);
+        }, 300);
     };
 })();
