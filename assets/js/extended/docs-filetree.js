f756fb546d1eb4f843036f42a2398f3b8a317b16
Ilia Ross <ilia@ross.gdn>
2023-11-27 23:49:00 +0200

    Fix left menu scroll into view

--- assets/js/extended/docs-filetree.js
+++ assets/js/extended/docs-filetree.js
@@ -1,36 +1,47 @@
-(function() {
+(function () {
     // Attach docs filetree menu to the right
     // container depending on the screen size
-    window.addEventListener("resize", function(e) {
-        const asideMenu = document.querySelector('.docs-menu'),
-            asideMobile = document.querySelector('.asideMobile .inner');
+    const containerSelector = ".docs-menu > .docs-menu-content";
+    window.addEventListener("resize", function (e) {
+        const asideMenu = document.querySelector(".docs-menu"),
+            asideMobile = document.querySelector(".asideMobile .inner");
 
         // Desktop mode
         if (asideMenu && asideMenu.offsetParent) {
-            const contentActiveContent = asideMobile.querySelector('.docs-menu-content');
-            contentActiveContent && asideMenu.append(contentActiveContent)
+            const contentActiveContent = asideMobile.querySelector(".docs-menu-content");
+            contentActiveContent && asideMenu.append(contentActiveContent);
             // Mobile mode
         } else {
-            const contentActiveContent = asideMenu.querySelector('.docs-menu-content');
-            contentActiveContent && asideMobile.append(contentActiveContent)
+            const contentActiveContent = asideMenu.querySelector(".docs-menu-content");
+            contentActiveContent && asideMobile.append(contentActiveContent);
         }
         document.dispatchEvent(new Event("scroll"));
     });
     window.dispatchEvent(new Event("resize"));
 
     // Scroll filetree into view
-    const closestDetails = document.querySelector('.docs-menu details .active');
-    closestDetails && closestDetails.closest('details').setAttribute('open', 'open');
-    // setTimeout(function() {
-    // }, (3e1 + 4e1) * 3)
-    if (closestDetails && !isVisibleInContainer(closestDetails, document.querySelector('.docs-menu-content'))) {
-        closestDetails.scrollIntoView({ behavior: "instant", block: 'center', inline: 'nearest' });
+    const closestDetails = document.querySelector(".docs-menu details .active"),
+        scrollToElementInContainer = function (element, container) {
+            const containerRect = container.getBoundingClientRect(),
+                elementRect = element.getBoundingClientRect(),
+                relativeTop = elementRect.top - containerRect.top,
+                scrollPosition = relativeTop + container.scrollTop;
+
+            // Scroll to the element
+            container.scrollTop = scrollPosition;
+        };
+    closestDetails && closestDetails.closest("details").setAttribute("open", "open");
+    if (
+        closestDetails &&
+        !isVisibleInContainer(closestDetails, document.querySelector(".docs-menu-content"))
+    ) {
+        scrollToElementInContainer(closestDetails, document.querySelector(containerSelector));
     }
     // When searching control related content
-    document.getElementById('searchInput').addEventListener('keyup', function(event) {
-        document.querySelector('.docs-menu-content').scrollTop = 0;
+    document.getElementById("searchInput").addEventListener("keyup", function (event) {
+        document.querySelector(".docs-menu-content").scrollTop = 0;
         if (closestDetails && !this.value && !window.matchMedia("(max-width: 900px)").matches) {
-            closestDetails.scrollIntoView({ behavior: "instant", block: 'center', inline: 'nearest' });
+            scrollToElementInContainer(closestDetails, document.querySelector(containerSelector));
         }
-    });    
+    });
 })();
