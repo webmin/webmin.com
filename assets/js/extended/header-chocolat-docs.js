6dceeca10564fcc44a2ba1dc916214821861cf7c
Ilia Ross <ilia@ross.gdn>
2024-02-12 01:00:47 +0200

    Rework to do screenshots right (no need for reloads)

--- assets/js/extended/header-chocolat-docs.js
+++ assets/js/extended/header-chocolat-docs.js
@@ -1,38 +1,52 @@
+// Init Chocolat for stand alone images in docs
 window.addEventListener("DOMContentLoaded", () => {
-    setTimeout(function () {
-        Chocolat(document.querySelectorAll('.post-content a[href*="/screenshots/"]'), {
-            allowFullScreen: false,
-            imageSize: "contain",
-            loop: false,
-            fullScreen: false,
-            imageSource: "href",
-            allowZoom: false,
-            imageSize: "scale-down",
-            afterInitialize: function afterInitialize() {
-                document.documentElement.classList.add("overflow-hidden");
-                document.querySelectorAll(".header, .main").forEach(function(el) {
-                    el.classList.add("disabled");
-                });
-            },
-            afterClose: function afterClose() {
-                document.documentElement.classList.remove("overflow-hidden");
-                document.querySelectorAll(".header, .main").forEach(function(el) {
-                    el.classList.remove("disabled");
-                });
-            },
-        });
-        document.addEventListener("keydown", function (event) {
-            if (event.key === "Escape" && document.body.classList.contains("chocolat-open")) {
-                event.preventDefault();
-                document.querySelector(".chocolat-layout .chocolat-close").click();
+    let chocolat;
+    const chocolatOptions = {
+        allowFullScreen: false,
+        imageSize: "contain",
+        loop: false,
+        fullScreen: false,
+        imageSource: "href",
+        allowZoom: false,
+        imageSize: "scale-down",
+        afterInitialize: function afterInitialize() {
+            document.documentElement.classList.add("overflow-hidden");
+            document.querySelectorAll(".header, .main").forEach(function (el) {
+                el.classList.add("disabled");
+            });
+        },
+        afterClose: function afterClose() {
+            document.documentElement.classList.remove("overflow-hidden");
+            document.querySelectorAll(".header, .main").forEach(function (el) {
+                el.classList.remove("disabled");
+            });
+        },
+    };
+    document.addEventListener("keydown", function (event) {
+        if (event.key === "Escape" && document.body.classList.contains("chocolat-open")) {
+            event.preventDefault();
+            document.querySelector(".chocolat-layout .chocolat-close").click();
+        }
+    });
+    document.querySelector("#theme-toggle").addEventListener(
+        "click",
+        function () {
+            if (document.querySelector(".post-content img.loading")) {
+                chocolat.destroy();
+                setTimeout(function () {
+                    chocolat = Chocolat(
+                        document.querySelectorAll('.post-content a[href*="/screenshots/"]'),
+                        chocolatOptions
+                    );
+                }, 100);
             }
-        });
-        document.querySelector("#theme-toggle").addEventListener(
-            "click",
-            function () {
-                document.querySelector(".post-content img.loading") && location.reload();
-            },
-            false
+        },
+        false
+    );
+    setTimeout(function () {
+        chocolat = Chocolat(
+            document.querySelectorAll('.post-content a[href*="/screenshots/"]'),
+            chocolatOptions
         );
     }, 100);
 });
